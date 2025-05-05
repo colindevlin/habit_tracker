bc58844b7cbdd24fc4cdaa44892a5cbc3f1bf5b3
Colin Devlin <dcolin.devlin@gmail.com>
2025-05-05 15:41:53 -0600

    first code commit

--- habit_tracker.py
+++ habit_tracker.py
@@ -12,3 +12,47 @@
 import sys
 import datetime
 
+main_menu_actions =[
+    "New habit",
+    "Log habit",
+    "Habit stats",
+    "Quit",
+]
+
+habits = [
+    {
+        "habit_name": "exercise",
+        "frequency": "daily",
+    }
+]
+
+
+def new_habit():
+    habit_input = input("Add a habit to track: ")
+    frequency_input = input("How often will you do it?: ")
+    habits.append({"habit_name": habit_input, "frequency": frequency_input})
+    return habits
+
+functions_dict = {
+    "New habit": new_habit,
+    # "Habit stats": habit_stats,
+    "Quit": sys.exit
+}
+
+def main_menu():
+    for index, action in enumerate(main_menu_actions, start=1):
+        print(f"{index}. {action}") # Print main menu
+    main_menu_input = input("Select an action: ")
+
+    main_menu_selection = int(main_menu_input)
+    if 1 <= main_menu_selection <= len(main_menu_actions):
+        action = main_menu_actions[main_menu_selection - 1]
+        functions_dict[action]()
+    else:
+        print("Invalid, try again.")
+
+
+
+
+# <----- main loop ----->
+main_menu()
