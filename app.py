03960d9192661e65984c1e6d0106c9e1e60f1cba
Colin Devlin <dcolin.devlin@gmail.com>
2025-05-28 07:12:05 -0600

    finished /log db refactor

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
 import datetime
-from models import Habit, session
+from models import Habit, HabitLog, session
 
 app = Flask(__name__)
 
-habits = [
-    {
-        'habit_name': 'exercise',
-        'frequency': 'daily',
-        'is_done': False,
-        'habit_streak': 0,
-        'log_dates': [],
-    }
-]
+# habits = [
+#     {
+#         'habit_name': 'exercise',
+#         'frequency': 'daily',
+#         'is_done': False,
+#         'habit_streak': 0,
+#         'log_dates': [],
+#     }
+# ]
 
 @app.route('/')
 def home_page():
@@ -42,34 +42,44 @@ def new_habit():
 
 @app.route('/view_list')
 def view_list():
-    habit_list = session.query('habit')
-
-    return render_template('view_list.html', habits=habit_list)
+    habit_list = session.query(Habit).all()
+    return render_template('view_list.html', habit_list=habit_list)
 
 @app.route('/log', methods=['GET', 'POST'])
 def log_habit():
-    if request.method == 'POST':
-        habit_index = int(request.form.get('habit_to_log'))
-        habit_logged = habits[habit_index]
-        date_logged = datetime.date.today()
-        date_logged_str = date_logged.isoformat()
-        if habit_logged['log_dates'] and habit_logged['log_dates'][-1] == date_logged_str:
+    if request.method == 'GET':
+        all_habits = session.query(Habit).all()
+        return render_template('log.html', habits=all_habits)
+
+    elif request.method == 'POST':
+        print(request.form)
+        habit_id = int(request.form.get('habit_id_to_log'))
+        habit_logged = session.get(Habit, habit_id)
+
+        # check if logged today
+        today = datetime.date.today().isoformat()
+        existing_log = session.query(HabitLog).filter_by(habit_id=habit_logged.id, date_logged=today).first()
+        if existing_log:
             error_flag = "You already logged this today."
+            return render_template('log.html', habit=habit_logged, error_flag=error_flag)
 
-            return render_template('log.html', habits=habits, error_flag=error_flag)
-
+        # no log today, update logs
         else:
-            habit_index = int(request.form.get('habit_to_log'))
-            habit_logged = habits[habit_index]
+            habit_id = int(request.form.get('habit_id_to_log'))
+            habit_logged = session.get(Habit, habit_id)
             date_logged = datetime.date.today()
             date_logged_str = date_logged.isoformat()
-            habit_logged['is_done'] = True
-            habit_logged['habit_streak'] += 1
+            habit_logged.is_done = True
+            habit_logged.habit_streak += 1
             formatted_date = datetime.datetime.strptime(date_logged_str, "%Y-%m-%d").strftime("%B %d, %Y")
-            habit_logged['log_dates'].append(formatted_date)
+            HabitLog(habit_id=habit_logged.id, date_logged=formatted_date)
+            new_log = HabitLog(habit_id=habit_logged.id, date_logged=date_logged)
+            session.add(new_log)
+            session.commit()
+            habit_logs = session.query(HabitLog).filter_by(habit_id=habit_logged.id).all()
 
-            return render_template('log.html', habits=habits, habit_logged=habit_logged, date_logged=formatted_date)
-    return render_template('log.html', habits=habits)
+            return render_template('log.html', habit_logged=habit_logged, date_logged=formatted_date, habit_logs=habit_logs)
+
 
 @app.route('/delete', methods=['GET', 'POST'])
 def delete_habit():
